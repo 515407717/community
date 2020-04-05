78d1f9def2403c5be37c148e487a6671f86d793e
malong <515407717@qq.com>
2020-04-05 14:55:35 +0800

    init repo

---
+++ src/main/java/net/mayalong/community/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package net.mayalong.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*
+@author mayl
+@create 2020-04-05 14:23
+*/
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
